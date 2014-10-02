4788c561c6bedddc06c30acf867c971f6535df78
Shaun Moss <shaun@astromultimedia.com>
2014-10-02 21:25:08 +1000

    Fixed paths

--- include/init.php
+++ include/init.php
@@ -8,18 +8,17 @@ ini_set('display_startup_errors', 0);
 ini_set('log_errors', 1);
 
 // find directory:
-$local = strpos($_SERVER['HTTP_HOST'], 'earthcalendar.info') === FALSE && strpos($_SERVER['HTTP_HOST'], 'earthcalendar.com') === FALSE;
+$local = strpos($_SERVER['HTTP_HOST'], 'earthiancalendar.info') === FALSE && strpos($_SERVER['HTTP_HOST'], 'earthcalendar.com') === FALSE;
 if ($local) {
-	$baseUrl = "http://earthcalendar.local";
-	$baseDir = "/Users/shaun/Dropbox/Projects/Calendars/Earth Calendar/earthcalender.info";
+	$baseUrl = "http://earthiancalendar.local";
 	$smUrl = "http://shaunmoss.local";
 }
 else {
-	$baseUrl = "http://earthcalendar.info";
-	$baseDir = "/var/aegir/platforms/earthcalendar.info";
+	$baseUrl = "http://earthiancalendar.info";
 	$smUrl = "http://shaunmoss.com";
 }
 
+$baseDir = substr(__DIR__, 0, strlen(__DIR__) - strlen('/include'));
 $imagesUrl = "$baseUrl/images";
 $imagesDir = "$baseDir/images";
 $libUrl = "$baseUrl/lib";
